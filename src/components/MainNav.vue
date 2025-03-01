5bf75976edec12b9756acdc775fe95ec7f353a9e
Mieszko Di Gregorio Botek <mieszkodgb@gmail.com>
2025-03-01 15:16:55 +0100

    fix navbar select effet

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
+import { RouterLink, useRoute } from 'vue-router';
+
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+};
 </script>
 
 <template>
@@ -8,25 +14,41 @@ import { cn } from '@/lib/utils'
   >
     <RouterLink 
       to="/"
-      class="text-sm font-medium transition-colors hover:text-primary"
+      :class="['text-sm font-medium transition-colors, hover:text-primary',
+              isActiveLink('/')
+              ? ''
+              : ' text-muted-foreground hover:text-primary',
+              ]"
       >
       Overview
     </RouterLink>
     <RouterLink 
       to="/list"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      :class="['text-sm font-medium transition-colors, hover:text-primary',
+              isActiveLink('/list')
+              ? ''
+              : ' text-muted-foreground hover:text-primary',
+              ]"
       >
       List view
     </RouterLink>
     <RouterLink 
       to="/chart"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      :class="['text-sm font-medium transition-colors, hover:text-primary',
+              isActiveLink('/chart')
+              ? ''
+              : ' text-muted-foreground hover:text-primary',
+              ]"
       >
       Chart view
     </RouterLink>
     <RouterLink 
       to="/settings"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      :class="['text-sm font-medium transition-colors, hover:text-primary',
+              isActiveLink('/settings')
+              ? ''
+              : ' text-muted-foreground hover:text-primary',
+              ]"
       >
       Setting view
     </RouterLink>
